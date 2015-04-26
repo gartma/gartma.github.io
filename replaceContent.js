37d2e6d5a96b85ebb149bfb0f3f558edf0d393d0
Max Gartner <gartma01@luther.edu>
2015-04-26 16:47:49 -0500

    fixing replaceContent.js target website

--- replaceContent.js
+++ replaceContent.js
@@ -1,21 +1,26 @@
 replaceContent = function(newContent, targetDivId) {
+	var request = new XMLHttpRequest()
 
-    var request = new XMLHttpRequest()
-    var theUrl = "http://webpages.charter.net/mgartner/"+newContent
-    
-    request.onreadystatechange = function () {
-        if ( request.readyState == 4 ) {
-            if ( request.status == 200 ) {
+	var theUrl = "http://gartma.github.io/"+newContent
 
-                document.getElementById(targetDivId).innerHTML = request.responseText
-            }
-            if ( request.status == 404) {
-                document.getElementById(targetDivId).innerHTML = ""
-            }
-        }
-   }
+	request.onreadystatechange = function () {
+		if ( request.readyState == 4 ) {
+			if ( request.status == 200 ) {
+				document.getElementById(targetDivId).innerHTML = request.responseText
+
+			}
+
+			if ( request.status == 404) {
+
+				document.getElementById(targetDivId).innerHTML = ""
+			}
+		}
+
+	}
 
 
-    request.open("GET", theUrl, true)
+
+	request.open("GET", theUrl, true)
     request.send(null)
+
 }
