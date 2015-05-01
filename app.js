9acb2b20c4e4f333b4a21d30977a695b12c45ca8
Max Gartner <gartma01@luther.edu>
2015-05-01 10:53:55 -0500

    Updating website with angular and swapping out the old with the new!

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var app = angular.module('gartma', ['ngRoute']);
+
+
+app.directive('navBar', function(){
+  return {
+    restrict: 'E',
+    templateUrl: '..\\views\\nav-bar.html'
+  };
+});
+
+app.config(function($routeProvider, $locationProvider){
+  $routeProvider
+    .when('/home', {
+      templateUrl: '\\views\\home.html'
+    })
+    .when('/portfolio', {
+      templateUrl: 'portfolio.html'
+    })
+    .when('/repositories', {
+      templateUrl: 'repositories.html'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+});
